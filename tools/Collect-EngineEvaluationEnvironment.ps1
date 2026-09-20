693b378fbb0979ac42b77a3f8ef2ddd46d6c90fc
Christopher W Peterson <codingcowboy2026@gmail.com>
2026-09-20 14:32:56 -0400

    fix: make runtime architecture expression PowerShell 5.1 compatible

--- tools/Collect-EngineEvaluationEnvironment.ps1
+++ tools/Collect-EngineEvaluationEnvironment.ps1
@@ -45,7 +45,7 @@ function Get-CommandVersion {
 
 # `$IsWindows and `$IsLinux are not available in Windows PowerShell 5.1.`n$IsWindowsPlatform = $env:OS -eq 'Windows_NT'`n$IsLinuxPlatform = (-not $IsWindowsPlatform) -and [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Linux)`n`n$Os = [ordered]@{
     platform = [System.Runtime.InteropServices.RuntimeInformation]::OSDescription
-    architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture.ToString()
+    architecture = ([System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture).ToString()
 }
 
 $Cpu = [ordered]@{
